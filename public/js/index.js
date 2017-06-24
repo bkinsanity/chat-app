c816474265a539448668c3099eb6b8dd413701ea
bkinsanity <bkinsanity@gmail.com>
2017-06-24 00:15:37 -0700

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -17,11 +17,12 @@ socket.on('newMessage', function (message) {
 
 jQuery("#message-form").on('submit', function(e) {
     e.preventDefault();
+    var messageTextBox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function() {
-
+        messageTextBox.val('');
     });
 })
 
@@ -39,14 +40,17 @@ locationButton.on('click', function() {
     if (!navigator.geolocation)
         return alert('Geolocation not supported by your browser.');
     
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function(position) {
         //console.log(position);
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
 
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
